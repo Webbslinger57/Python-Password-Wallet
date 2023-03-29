000b8bb6433c41154cd2792aa8379b94c925fd14
Webbslinger57 <webbslinger57@gmail.com>
2023-03-28 19:53:30 -0500

    Added Additional Navigation
    Added a back button as well as a few minor items

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark",
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 app = customtkinter.CTk()
-app.geometry("500x400")
+app.geometry("520x500")
 app.resizable(False, False)
 
 
@@ -46,21 +46,31 @@ def main():
 def home_view():
     app.title("Home")
     
+    def back_click():
+        home_frame.destroy()
+        main()
+    
     def add_password_click():
         home_frame.destroy()
         add_password_view() 
         
-    def view_passwords_click():
-        print("Viewing Passwords")
+    def search_click():
+        print("Searching for:", entry_1.get())
     
     home_frame = customtkinter.CTkFrame(master=app)
-    home_frame.pack(pady=20, padx=60, fill="both", expand=True)
+    home_frame.pack(pady=20, padx=20, fill="both", expand=True)
     
-    add_password_btn = customtkinter.CTkButton(master=home_frame, text="Add Password", command=add_password_click)
-    add_password_btn.pack(pady=10, padx=10)
+    back_btn = customtkinter.CTkButton(master=home_frame, text="Back", command=back_click)
+    back_btn.grid(row=0, column=0, padx=10, pady=10)
     
-    view_passwords_btn = customtkinter.CTkButton(master=home_frame, text="View Passwords", command=view_passwords_click)
-    view_passwords_btn.pack(pady=10, padx=10)
+    add_password_btn = customtkinter.CTkButton(master=home_frame, text="Add Account", command=add_password_click)
+    add_password_btn.grid(row=0, column=2, padx=10, pady=10)
+    
+    entry_1 = customtkinter.CTkEntry(master=home_frame, placeholder_text="Search Accounts")
+    entry_1.grid(row=1, column=1, padx=10, pady=10)
+    
+    view_passwords_btn = customtkinter.CTkButton(master=home_frame, text="Search", command=search_click)
+    view_passwords_btn.grid(row=2, column=1, padx=10, pady=10)
 
 
 ##################################################################################################
@@ -68,6 +78,10 @@ def home_view():
 ##################################################################################################
 def add_password_view():
     app.title("Add Password")
+    
+    def back_click():
+        frame_1.destroy()
+        home_view()
     
     def button_callback():
         print("Adding Note:", note_entry.get())
@@ -80,7 +94,10 @@ def add_password_view():
     frame_1 = customtkinter.CTkFrame(master=app)
     frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-    label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT)
+    back_btn = customtkinter.CTkButton(master=frame_1, text="Back", command=back_click)
+    back_btn.grid(row=0, column=0, padx=10, pady=10)
+
+    label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text="Add a New Account")
     label_1.pack(pady=10, padx=10)
 
     note_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="note")
